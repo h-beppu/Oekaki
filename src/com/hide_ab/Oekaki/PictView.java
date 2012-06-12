d8070d86544279c4172a1c3b6cb20e151cb65579
beppu <beppu@c13ad3e7-40b1-2f46-82f6-42d70fb37b30>
2012-06-12 07:56:31 +0000

    2011/09/06 01:58:40 Surface版、ほぼ決定。
    git-svn-id: http://192.168.0.175/repos/Oekaki@5 c13ad3e7-40b1-2f46-82f6-42d70fb37b30

--- src/com/hide_ab/Oekaki/PictView.java
+++ src/com/hide_ab/Oekaki/PictView.java
@@ -18,16 +18,18 @@ public class PictView extends  SurfaceView implements SurfaceHolder.Callback, Ru
 	private int type = 0;				// イベントのタイプ
 	private float posx = 0.0f;			// イベントが起きたX座標
 	private float posy = 0.0f;			// イベントが起きたY座標
-	private final int REPEAT_INTERVAL = 2;
+	private final int REPEAT_INTERVAL = 50;
 	private final int BRUSH_SIZE = 4;
+
+	private SurfaceHolder holder;
+	private Thread thread = null;		// スレッドクラス
 
 	private Path path = null;			// パス
 	private Paint paint = null;			// 描画用
-
-	private Thread thread = null;		// スレッドクラス
-	private static final String LOG = "MainSurfaceView";
-
 	private Bitmap bmp = null;
+	private Canvas bmpCanvas;
+
+	private static final String LOG = "MainSurfaceView";
 
 	public PictView(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -36,32 +38,41 @@ public class PictView extends  SurfaceView implements SurfaceHolder.Callback, Ru
 		setOnTouchListener(this);
 
 		// SurfaceView描画に用いるコールバックを登録
-		getHolder().addCallback(this);
+		holder = getHolder();
+		holder.addCallback(this);
 
 		// 描画用の準備
 		paint = new Paint();
-		//アンチエイリアスを有効にする
+		// アンチエイリアスを有効にする
 		paint.setAntiAlias(true);
-		//青色、透明度100
+		// 白色、透明度100
 		paint.setColor(Color.WHITE);
 //		paint.setColor(Color.argb(100, 0, 0, 255));
-		//線のみ(塗りつぶさない)
+		// 線のみ(塗りつぶさない)
 		paint.setStyle(Paint.Style.STROKE);
-		//線の太さ8
+		// 線の太さ
 		paint.setStrokeWidth(BRUSH_SIZE);
-		//線の両端を丸くする
+		// 線の両端を丸くする
 		paint.setStrokeCap(Paint.Cap.ROUND);
-		//線のつなぎ目を丸くする
+		// 線のつなぎ目を丸くする
 		paint.setStrokeJoin(Paint.Join.ROUND);
 
 		Log.d(LOG, "start");
 	}
 
-	public boolean onTouch(View view, MotionEvent event) {
-		type = event.getAction();	//イベントのタイプ
-		posx = event.getX();		//イベントが起きたX座標
-		posy = event.getY();		//イベントが起きたY座標
-		return true;
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		bmpCanvas = new Canvas(bmp);
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		Log.d(LOG, "create");
+
+		// スレッド開始
+		thread = new Thread(this);
+		thread.start();
 	}
 
 	@Override
@@ -71,37 +82,41 @@ public class PictView extends  SurfaceView implements SurfaceHolder.Callback, Ru
 	}
 
 	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		Log.d(LOG, "create");
-
-		// Canvasの確保(1stバッファ)
-		Canvas canvas = holder.lockCanvas();
-		canvas.drawColor(Color.BLUE);
-		// Canvasの解放
-		holder.unlockCanvasAndPost(canvas);
-
-		// Canvasの確保(2ndバッファ)
-		canvas = holder.lockCanvas();
-		canvas.drawColor(Color.BLUE);
-		// Canvasの解放
-		holder.unlockCanvasAndPost(canvas);
-
-/*
-		int w = getWidth();
-		int h = getHeight();
-		bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-		canvas = new Canvas(bmp);
-		canvas.drawColor(Color.BLUE);
-*/
-
-		// スレッド開始
-		thread = new Thread(this);
-		thread.start();
-	}
-
-	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		thread = null;
+	}
+
+	public boolean onTouch(View view, MotionEvent event) {
+		type = event.getAction();	//イベントのタイプ
+		posx = event.getX();		//イベントが起きたX座標
+		posy = event.getY();		//イベントが起きたY座標
+
+		// イベントのタイプごとに処理を設定
+		switch(type){
+			case MotionEvent.ACTION_DOWN:	//最初のポイント
+				// パスを初期化
+				path = new Path();
+				// パスの始点へ移動
+				path.moveTo(posx, posy);
+				break;
+			case MotionEvent.ACTION_MOVE:	//途中のポイント
+				// ひとつ前のポイントから線を引く
+				path.lineTo(posx, posy);
+				break;
+			case MotionEvent.ACTION_UP:		//最後のポイント
+				// ひとつ前のポイントから線を引く
+				path.lineTo(posx, posy);
+				break;
+			default:
+				break;
+		}
+
+		// 描画バッファにパスを描画する
+		if(path != null) {
+			bmpCanvas.drawPath(path, paint);
+		}
+
+		return true;
 	}
 
 	@Override
@@ -109,47 +124,20 @@ public class PictView extends  SurfaceView implements SurfaceHolder.Callback, Ru
 		// Runnableインターフェースをimplementsしているので、runメソッドを実装する
 		// これは、Threadクラスのコンストラクタに渡すために用いる。
 		while(thread != null) {
-/*
-			if(bmp == null) {
-				continue;
+			// Canvasの確保
+			Canvas canvas = holder.lockCanvas();
+			if(canvas != null) {
+				// 描画バッファのデータを書き出し
+				canvas.drawBitmap(bmp, 0, 0, null);
+				// Canvasの解放
+				holder.unlockCanvasAndPost(canvas);
 			}
-*/
 
+			// ウェイト
 			try {
 				thread.sleep(REPEAT_INTERVAL);
 			}
 			catch(InterruptedException e) {
-				Log.d(LOG, "sleep fail.");
-			}
-
-			// Canvasの確保
-			Canvas canvas = getHolder().lockCanvas();
-			if(canvas != null) {
-//				canvas.drawBitmap(bmp, 0, 0, null);
-
-				// イベントのタイプごとに処理を設定
-				switch(type){
-					case MotionEvent.ACTION_DOWN:	//最初のポイント
-						//パスを初期化
-						path = new Path();
-						//パスの始点へ移動
-						path.moveTo(posx, posy);
-						break;
-					case MotionEvent.ACTION_MOVE:	//途中のポイント
-						//ひとつ前のポイントから線を引く
-						path.lineTo(posx, posy);
-						break;
-					case MotionEvent.ACTION_UP:		//最後のポイント
-						//ひとつ前のポイントから線を引く
-						path.lineTo(posx, posy);
-						break;
-					default:
-						break;
-				}
-				// 描画
-				canvas.drawPath(path, paint);
-				// Canvasの解放
-				getHolder().unlockCanvasAndPost(canvas);
 			}
 		}
 	}
